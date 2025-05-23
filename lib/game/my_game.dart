148d92b2171b430d667fc4bf099f14acc1c0afc8
Muffin.ph <79498018+JesusPeraltaH@users.noreply.github.com>
2025-05-22 19:10:57 -0700

    cleanup

--- lib/game/my_game.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flame/game.dart';
-
-class MyGame extends FlameGame {
-  @override
-  Future<void> onLoad() async {
-    // Aquí puedes cargar tus assets y configurar el juego
-  }
-}
--- lib/widgets/components.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flame/collisions.dart';
-import 'package:flame/components.dart';
-
-class HouseHitbox extends PositionComponent with CollisionCallbacks {
-  HouseHitbox(List<Vector2> vertices) : super() {
-    add(PolygonHitbox(vertices));
-  }
-}
